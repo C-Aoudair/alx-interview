60d3a6167a46a8c584453f742491ea829efda19a
C-Aoudair <aoudair16031999@gmail.com>
2024-06-07 23:16:15 +0100

    complete the lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module contains the functioin that solvse the lockboxes problem"""
+
+
+def canUnlockAll(boxes):
+    """ This function takes list of lists as boxes and return true
+        if all boxes can be opened
+    """
+    openedBoxes = []
+    boxesQueue = []
+    boxesQueue.append(0)
+
+    while len(boxesQueue) != 0:
+        box = boxesQueue.pop(0)
+        openedBoxes.append(box)
+
+        for neighbour in boxes[box]:
+            if neighbour not in boxesQueue and neighbour not in openedBoxes:
+                boxesQueue.append(neighbour)
+
+    if len(openedBoxes) == len(boxes):
+        return True
+    return False
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+this is for the lockboxes interview problem
