9e1373643dc667d0b85057c386327c0e407b2701
C-Aoudair <aoudair16031999@gmail.com>
2024-06-29 11:44:09 +0100

    change the algorithm of the solution to machine state

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,20 +8,20 @@ def validUTF8(data: list) -> bool:
     def is_continuation_byte(byte):
         return (byte >> 6) == 0b10
 
-    numOfBytes = 0 # The number of bytes that the each utf8 character will be
+    numOfBytes = 0  # The number of bytes that the each utf8 character will be
 
     for byte in data:
-        byte = byte & 0xFF # Ensure that the byte is less than 255
+        byte = byte & 0xFF  # Ensure that the byte is less than 255
 
-        if numOfBytes == 0: # Recognize the utf-8 bytes pattern
+        if numOfBytes == 0:  # Recognize the utf-8 bytes pattern
 
-            if (byte >> 5) == 0b110: # For two bytes
+            if (byte >> 5) == 0b110:  # For two bytes
                 numOfBytes = 1
-            elif (byte >> 4) == 0b1110: # For three bytes
+            elif (byte >> 4) == 0b1110:  # For three bytes
                 numOfBytes = 2
-            elif (byte >> 3) == 0b11110: # For four bytes
+            elif (byte >> 3) == 0b11110:  # For four bytes
                 numOfBytes = 3
-            elif (byte >> 7):
+            elif byte >> 7:
                 return False
 
         else:
@@ -29,4 +29,4 @@ def validUTF8(data: list) -> bool:
                 return False
             numOfBytes -= 1
 
-    return numOfBytes == 0 # Ensure that all continuation bytes was checked
+    return numOfBytes == 0  # Ensure that all continuation bytes was checked
