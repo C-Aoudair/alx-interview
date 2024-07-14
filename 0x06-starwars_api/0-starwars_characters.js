049c3eb2aa2ee415cda586f1e9a60d36fffa4338
C-Aoudair <aoudair16031999@gmail.com>
2024-07-15 00:07:03 +0100

    add the second version of the solution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,25 +2,41 @@
 
 const request = require('request');
 
+function requestPromise (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return reject(new Error('something went wrong'));
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+}
+
 const id = process.argv[2];
 
-request(
-  `https://swapi-api.alx-tools.com/api/films/${id}/`,
-  async function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const peopleEndPoints = JSON.parse(body).characters;
+async function fetchCharacterNames () {
+  requestPromise(`https://swapi-api.alx-tools.com/api/films/${id}/`)
+    .then((filmBody) => {
+      const peopleEndPoints = filmBody.characters;
 
-      for (const endPoint of peopleEndPoints) {
-        request(endPoint, (error, response, body) => {
-          if (!error && response.statusCode === 200) {
-            console.log(JSON.parse(body).name);
-          } else {
-            console.log(error);
-          }
-        });
-      }
-    } else {
-      console.error('Error:', error);
-    }
-  }
-);
+      let promiseChain = Promise.resolve();
+
+      peopleEndPoints.forEach((endPoint) => {
+        promiseChain = promiseChain
+          .then(() => requestPromise(endPoint))
+          .then((characterBody) => {
+            console.log(characterBody.name);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+      return promiseChain;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+fetchCharacterNames();
