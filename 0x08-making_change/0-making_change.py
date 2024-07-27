f54a79889575c7d2f11424157976588520dea44f
C-Aoudair <aoudair16031999@gmail.com>
2024-07-27 14:29:41 +0100

    clean up the makeChange function as well as the directory

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,17 +3,24 @@
 
 
 def makeChange(coins, total):
-    """makeChange function"""
-    numOfCoins = 0
-    while(total > 0):
-        status = False
-        for coin in reversed(sorted(coins)):
-            if coin <= total:
-                numOfCoins += 1
-                total -= coin
-                status = True
-                break
-        if not status:
-            return -1
+    """
+    Determines the minimum number of coins needed to make the given total.
 
-    return numOfCoins
+    Args:
+        coins (list of int): A list of coin denominations available.
+        total (int): The total amount to be achieved using the coins.
+
+    Returns:
+        int: The minimum number of coins needed to make the total.
+             Returns -1 if the total cannot be achieved
+             with the given denominations.
+    """
+    coins.sort(reverse=True)
+    coin_count = 0
+
+    for coin in coins:
+        while total >= coin:
+            coin_count += 1
+            total -= coin
+
+    return coin_count if total == 0 else -1
