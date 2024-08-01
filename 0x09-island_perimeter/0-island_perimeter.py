a6997c72964ad7926e89cd7f9319ea298c89d6cf
C-Aoudair <aoudair16031999@gmail.com>
2024-08-01 01:32:45 +0100

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,10 +4,16 @@
 
 def island_perimeter(grid):
     """takes a grid and compute the perimeter of the land island of it"""
+    if len(grid) > 100 or len(grid[0]) > 100:
+        return
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j]:
+            if grid[i][j] == 0:
+                continue
+
+            elif grid[i][j] == 1:
                 cell_perimeter = 0
                 try:
                     if not grid[i][j + 1]:
@@ -32,5 +38,7 @@ def island_perimeter(grid):
                 except IndexError:
                     cell_perimeter += 1
                 perimeter += cell_perimeter
+            else:
+                return
 
     return perimeter
