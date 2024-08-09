7a70e9b8de8c77a3655a50c6509b03c76c1ec34f
C-Aoudair <aoudair16031999@gmail.com>
2024-08-09 02:28:39 +0100

    add the first implementation of the solution for prime game problem

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Defines isWinner function the solution of prime game problem"""
+
+
+def isWinner(x, nums):
+    """ isWinner function"""
+    Maria = 0
+    Ben = 0
+
+    for n in nums:
+        primes = [True for _ in range(n + 1)]
+
+        p = 2
+        Maria_turn = True
+        while(p * p <= n):
+            if (primes[p]):
+                for i in range(p*p, n + 1, p):
+                    primes[i] = False
+                Maria_turn = not Maria_turn
+
+                p += 1
+
+        if (Maria_turn):
+            Ben += 1
+        else:
+            Maria += 1
+
+    if Maria > Ben:
+        return "Maria"
+    if Maria < Ben:
+        return "Ben"
+    return None
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,1 @@
+the Prime game problem
