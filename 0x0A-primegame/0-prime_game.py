081f09c64d32a015e7097b352c1775c262045fbc
C-Aoudair <aoudair16031999@gmail.com>
2024-08-09 02:38:25 +0100

    add the number of round represented with x argument to the conditions

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,24 +6,28 @@ def isWinner(x, nums):
     """ isWinner function"""
     Maria = 0
     Ben = 0
-
+    
     for n in nums:
-        primes = [True for _ in range(n + 1)]
+        rounds_number = 1
+        while rounds_number <= x:
+            primes = [True for _ in range(n + 1)]
 
-        p = 2
-        Maria_turn = True
-        while(p * p <= n):
-            if (primes[p]):
-                for i in range(p*p, n + 1, p):
-                    primes[i] = False
-                Maria_turn = not Maria_turn
+            p = 2
+            Maria_turn = True
+            while(p * p <= n):
+                if (primes[p]):
+                    for i in range(p*p, n + 1, p):
+                        primes[i] = False
+                    Maria_turn = not Maria_turn
 
-                p += 1
+                    p += 1
 
-        if (Maria_turn):
-            Ben += 1
-        else:
-            Maria += 1
+            if (Maria_turn):
+                Ben += 1
+            else:
+                Maria += 1
+            
+            rounds_number += 1
 
     if Maria > Ben:
         return "Maria"
